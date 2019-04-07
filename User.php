133a075116c47af9e4c50ce24fe71d3ff103292e
ignzdave <david.sutanto@live.com>
2019-04-07 21:13:53 +0800

    Update User.php

--- User.php
+++ User.php
@@ -51,26 +51,28 @@ class User {
 	function create() {
 		$this->connection = new Connection();
 		$conn = $this->connection->openConnection();
-
 		$insert = $conn->prepare("INSERT INTO [dbo].[User] (FullName, Email, Gender) VALUES (:name, :email, :gender)");
+
 		try {
 			$conn->beginTransaction();
 			$result = $insert->execute(array(
-				'FullName' => $this->getFullName(),
-				'Email' => $this->getEmail(),
-				'Gender' => $this->getGender()
+				':name' => $this->getFullName(),
+				':email' => $this->getEmail(),
+				':gender' => $this->getGender()
 			));
 
-			$conn->commit();
-			
+			if ($result) {
+				$conn->commit();
+				echo "<i style='color:green'>User Saved Successfully..! <i>";
+			} else {
+				echo "<i style='color:red'>There are some problem while saving the Data...! <i>";
+			}
 		} catch (PDOExecption $e) {
 			$conn->rollback();
 			print "Error!: " . $e->getMessage() . "</br>";
 		}
 
-		print_r($insert);
 		$this->connection->closeConnection();
-
 	}
 
 }
